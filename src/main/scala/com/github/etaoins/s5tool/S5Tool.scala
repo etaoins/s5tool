c9ac82ce9623b5f5af5e84dde73fa258498482db
Ryan Cumming <etaoins@gmail.com>
2012-10-09 21:58:59 -0700

    Add option parsing

--- src/main/scala/com/github/etaoins/s5tool/S5Tool.scala
+++ src/main/scala/com/github/etaoins/s5tool/S5Tool.scala
@@ -1,4 +1,20 @@
 package com.github.etaoins.s5tool
 
+import scopt.immutable._
+
+case class Config(
+  filesystemRoot : String,
+  bucketName : String
+)
+
 object S5Tool extends App {
+  val parser = new OptionParser[Config]("s5tool", "0.0,1") {
+    def options = Seq(
+      arg("<source>", "source directory") { (v: String, c: Config) => c.copy(filesystemRoot = v) },
+      arg("<bucket>", "S3 bucket name") { (v: String, c: Config) => c.copy(bucketName = v) }
+    )
+  }
+
+  parser.parse(args, Config("", "")) map { config =>
+  }
 }
